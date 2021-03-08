043c4e3d8233037f2ea29014539c5159b24ebd3d
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-03-08 22:35:21 +0800

    Add files via upload

---
+++ TestDemo35.java
@@ -0,0 +1,13 @@
+public class TestDemo35 {
+    /**
+     * 装箱（装包）：简单类型变为包装类类型
+     * 拆箱（拆包）：包装类类型变为简单类型
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        Integer a = 200;
+        Integer b = 200;
+        System.out.println(a == b);
+    }
+}
