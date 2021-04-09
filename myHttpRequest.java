f11c5ee830548d3f73e69050660a21c9821ca62d
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-04-09 20:43:01 +0800

    自定义浏览器request端

--- myHttpRequest.java
+++ myHttpRequest.java
@@ -1,54 +1,38 @@
-package HTTP;
+package myHttp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 需求：
- *  1. 如果是/404，则显示没有找到页面
- *  2. 如果是/302，则跳转到指定的页面，如跳转到百度
- *  3. 如果是/200，则显示正常页面
- */
 public class myHttpRequest {
-    //1.首行信息【方法+URL+版本号】,然后对首行信息进行封装
+    //首行[方法+URL+版本号]
     //方法
-    private String method;
-    //url
-    private String url;
+    private  String method;
+    //URL
+    private  String url;
     //版本号
     private String version;
-    //2.获取URL里面的参数，因为url里面的参数可能有多个，而且是以key-value的形式，所以使用map
-    Map<String,String> parments = new HashMap<>();
 
-    //3.获取header信息
-    Map<String,String> requsetHeader = new HashMap<>();
+    //获取URL里面的参数
+    Map<String,String> parents = new HashMap<>();
+    //header
+    Map<String,String> requestHeader = new HashMap<>();
+    //空行
+    //body其实也是key1=value1&key2=value2的键值对形式，所以也需要借助Map,然后提供它的构造方法
+    Map<String,String> requestBody = new HashMap<>();
 
-    //4.空行
-
-    //5.body（暂不考虑）
-
-    //初始化内容，想要读取request的信息，就需要借助BufferedReader
+    //初始化方法,要读取request的内容必须借助BufferedRead
     public void init(BufferedReader reader) throws IOException {
-        //首先读取到首行信息
+        //读取第一行
         String firstLine = reader.readLine();
-        //因为首行信息是按照“ ”（空格）进行分隔的，所以我们就按照空格进行分隔
-        String[] firstLineArr = firstLine.split(" ");
-        //然后对首行信息里面的方法进行初始化
-        this.method = firstLineArr[0];
-        //初始化URL
-        this.url = firstLineArr[1];
-        //初始化版本号
-        this.version = firstLineArr[2];
-
-
-        //读取URL里面的参数
-        //url如：http://182.254.116.116/d?dn=6ea62b48a6cc721df07b8f9b03956ad7&id=2046&ttl=1
-        //我们可以看到从？以后他都是以“&”进行分隔的一个个key=value的对象，也就是/s？key1=val1&key2=val2……
-        //创建一个方法去读取URL里面的参数,方法里面的参数是把整个URL传进去
-        processURL(this.url);
-
+        //然后将读取的第一行数据按照空格进行分隔
+        String[] firsrLineArr = firstLine.split(" ");
+        this.method = firsrLineArr[0];
+        this.url = firsrLineArr[1];
+        this.version = firsrLineArr[2];
+        //然后对拿到的url进行处理
+        processUrl(this.url);
         //读取请求头
         //因为它的内容不止一条，所以要使用while循环
         //定义一个字符串，来存放每次读取的每一行数据
@@ -59,37 +43,57 @@ public class myHttpRequest {
             //然后header的每一行数据都是按照“: ”(冒号+空格的方式)，那么我们就按照（冒号+空格）的方式分隔读取到的每一行
             String[] lineArr = line.split(": ");
             //然后将获取到的数据存储在我们申请的map中，也就是requestHeader中
-            requsetHeader.put(lineArr[0],lineArr[1]);
-
+            requestHeader.put(lineArr[0],lineArr[1]);
         }
 
+        //4.读取body的值，首先得判断是post请求还是get请求，因为如果是get请求的话，body是空的,而且是在method中进行判断
+        //equalsIgnoreCase是忽略大小写的意思
+        if(this.getMethod().equalsIgnoreCase("post")){//表示body中有内容，进行赋值
+            //如果想要拿到body，首先得拿到body的长度，也就是Content-Length,也就是得知道body中有多少个字节
+            int contentLength = Integer.parseInt(this.getRequestHeader("Content-length"));
+            //然后将读取的字节放在一个char数组里面
+            char[] bodys = new char[contentLength];
+            //表示读取body信息
+            reader.read(bodys,0,contentLength);
+            //然后拿到body的字符串，如何将字符转换成字符串，使用new String()
+            //而拿到的字符串是这样的key=val1&key2=val2
+            String bodyStr = new String(bodys);
+            //那么就需要将其用“&”进行分隔，然后再用“=”进行分隔 然后放进body字典中
+            //首先遍历字符串用&进行分隔
+            for (String item:bodyStr.split("&")
+                 ) {
+                //然后在讲拿到的key=val形式按照“="进行分隔
+                String[] kvs = item.split("=");
+                //然后再将其放入body字典中
+                requestBody.put(kvs[0],kvs[1]);
 
-    }
 
-    //将URL里面的参数存放在parents中
-    private void processURL(String url) {
-        //首先将？之后的内容截取出来，也就是将
-        // /s？key1=val1&key2=val2……变成key1=val1&key2=val2
-        //使用String中的substring方法，也就是从？+1的位置开始截取完
-        //但是首先得判断URL里面有没有“？”问号
-        if(url.startsWith("?")) {
-            String newUrl = url.substring(url.indexOf("?") + 1);
-            //然后将新的newUrl用“&”进行分隔变成key=value的形式
-            String[] newUrlArr = newUrl.split("&");
-            //遍历这个新的newUrl
-            for (String kvs : newUrlArr) {
-                //然后newUrl里面的每一个key=value用“=”进行分隔
-                String[] item = kvs.split("=");
-                //然后放进map里面也就是parents里面
-                parments.put(item[0], item[1]);
             }
         }
-
     }
 
-    //如果外部类要去使用的话，我们必须提供get方法
+    private void processUrl(String url) {
+        //http://119.29.29.29/d?dn=baidu.com,
+        //首先得判断有没有问号
+        if (this.url.startsWith("?")) {
+            //然后将这个从？之后截取
+            String newUrl = this.url.substring(this.url.indexOf("?"));
+            //然后拿到新的url按照“&”进行分隔
+            String[] newUrlArr = newUrl.split("&");
+            //然后遍历newUrlArr中的数据，然后将其按照”=“进行分隔
+            for (String item:newUrlArr
+                    ) {
+                String[] items = item.split("=");
+                //然后将其放入map中
+                parents.put(items[0],items[1]);
+            }
+        }
+    }
+    //提供get方法
+
 
     public String getMethod() {
+
         return method;
     }
 
@@ -97,14 +101,22 @@ public class myHttpRequest {
         return url;
     }
 
+
     public String getVersion() {
+
         return version;
     }
-    //然后手动实现两个map的get方法
+    //手动构造两个map的get方法
     public String getParents(String key){
-        return this.parments.get(key);
+
+        return this.parents.get(key);
     }
     public String getRequestHeader(String key){
-        return this.requsetHeader.get(key);
+
+        return this.requestHeader.get(key);
+    }
+    //提供requestBody的get方法
+    public String getRequestBody(String key){
+        return this.requestBody.get(key);
     }
 }
