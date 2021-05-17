bfddb0e61bcc533a083d6e5a223fdf2afb66e558
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-05-17 23:29:18 +0800

    杨辉三角进阶

--- Demo5.java
+++ Demo5.java
@@ -1,35 +1,31 @@
-package Practice;
+package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
+/**
+ * 杨辉三角进阶
+ */
 public class Demo5 {
-
-    public static int[] smallestK(int[] arr, int k){
-        //申请一个大小为k的数组，来存放弹出的k个元素
-        int[] array = new int[k];
-        //首先判断数组是否为空，或者k是否为有效值
-        if(arr == null ||k <= 0){
-            return null;
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> pre = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            cur = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = cur;
         }
-
-        //然后将数组里面的所有数全部都放在优先级队列里面
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        for (int i = 0; i < arr.length; i++) {
-            priorityQueue.offer(arr[i]);
-        }
-
-        //然后根据PriorityQueue的特性，将队顶的k个元素弹出，并将其放入一个新的数组里面
-
-        for (int i = 0; i < k; i++) {
-            array[i] = priorityQueue.poll();
-        }
-        return array;
+        return cur;
     }
+
     public static void main(String[] args) {
-        int[] arr = {3,2,1};
-        int[] ret = smallestK(arr,2);
-        System.out.println(Arrays.toString(ret));
+        List<Integer> list = getRow(5);
+        System.out.println(list);
     }
 }
