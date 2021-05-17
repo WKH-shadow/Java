817043f7d969db5012f7402fc3c8052ddf5efd6a
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-05-17 23:28:50 +0800

    最大子序和

--- Demo4.java
+++ Demo4.java
@@ -1,138 +1,32 @@
-package Practice;
+package LeetCode;
 
-import java.util.Stack;
-
-class TreeNode1 {
-    int val;
-    TreeNode1 left;
-    TreeNode1 right;
-    TreeNode1(int x) { val = x; }
-}
+/**
+ * 最大子序和：
+ * 1.首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
+ * 2.如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
+ * 3.如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
+ * 4.每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
+ * 时间复杂度：O(n)O(n)
+ *
+ */
 public class Demo4 {
-
-    //创建二叉树
-    public static TreeNode1 createTree(){
-        TreeNode1 node1 = new TreeNode1(1);
-        TreeNode1 node2 = new TreeNode1(2);
-        TreeNode1 node3 = new TreeNode1(3);
-        TreeNode1 node4 = new TreeNode1(4);
-        TreeNode1 node5 = new TreeNode1(5);
-        TreeNode1 node6 = new TreeNode1(6);
-        node1.left = node2;
-        node1.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node3.left = node6;
-        return node1;
-}
-
-      //二叉树的前序遍历递归实现
-    public static void preOrderTraver(TreeNode1 root){
-        if(root == null){
-            return;
-        }
-        System.out.print(root.val+" ");
-        preOrderTraver(root.left);
-        preOrderTraver(root.right);
-    }
-     //二叉树的前序遍历非递归实现
-    public static void preOrderTraverlNor(TreeNode1 root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode1> stack = new Stack<>();
-        TreeNode1 cur = root;
-        while (cur != null || !stack.isEmpty()){
-            while (cur != null) {
-                stack.push(cur);
-                System.out.print(cur.val + " ");
-                cur = cur.left;
+    public static int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for(int num: nums) {
+            if(sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
             }
-            TreeNode1 top = stack.pop();
-            cur = top.right;
+            ans = Math.max(ans, sum);
         }
-    }
-    //中序遍历递归实现
-    public static void inOrderTravel(TreeNode1 root){
-        if(root == null){
-            return;
-        }
-        inOrderTravel(root.left);
-        System.out.print(root.val+" ");
-        inOrderTravel(root.right);
+        return ans;
     }
 
-    //中序遍历非递归实现
-    public static void inOrderTravelNor(TreeNode1 root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode1> stack = new Stack<>();
-        TreeNode1 cur = root;
-        while (cur != null || !stack.isEmpty()){
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-
-            }
-            TreeNode1 top = stack.pop();
-            System.out.print(top.val+" ");
-            cur = top.right;
-        }
-    }
-
-    //后序遍历递归实现
-    public static void postOrderTravel(TreeNode1 root){
-        if(root == null){
-            return;
-        }
-        postOrderTravel(root.left);
-        postOrderTravel(root.right);
-        System.out.print(root.val+" ");
-    }
-
-    //后序遍历非递归实现
-    public static void postOrderTravelNor(TreeNode1 root) {
-        if (root == null) {
-            return;
-        }
-        Stack<TreeNode1> stack = new Stack<>();
-        TreeNode1 cur = root;
-        TreeNode1 pre = null;
-        while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
-            cur = stack.peek();
-            if(cur.right == null || cur.right == pre){
-                System.out.print(cur.val+" ");
-                stack.pop();
-                pre = cur;
-                cur = null;
-            }else {
-                cur = cur.right;
-            }
-        }
-    }
     public static void main(String[] args) {
-        TreeNode1 root = createTree();
-        System.out.println("=====前序遍历递归实现=====");
-        preOrderTraver(root);
-        System.out.println();
-        System.out.println("====前序遍历非递归实现====");
-        preOrderTraverlNor(root);
-        System.out.println();
-        System.out.println("====中序遍历递归实现====");
-        inOrderTravel(root);
-        System.out.println();
-        System.out.println("=====中序遍历非递归实现====");
-        inOrderTravelNor(root);
-        System.out.println();
-        System.out.println("====后序遍历递归实现====");
-        postOrderTravel(root);
-        System.out.println();
-        System.out.println("=====后序遍历非递归实现====");
-        postOrderTravelNor(root);
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int ret = maxSubArray(arr);
+        System.out.println(ret);
     }
 }
