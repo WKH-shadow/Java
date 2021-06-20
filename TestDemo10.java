83330cf396b4131b49b0a20112b5130c7645bf6f
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-06-20 20:19:33 +0800

    石头与宝石

--- TestDemo10.java
+++ TestDemo10.java
@@ -1,73 +1,54 @@
-package JavaPractice;
+package TestDemo;
 
-import java.util.Stack;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * 实现一个最小栈
- * 入栈：
- * 1.首先需要两个栈 一个stack 一个minStack
- * 2.对于stack来说，每一次都会存放数据
- * 3.对于minStack来说
- * 3.1如果是第一次存放，那么肯定会将stack中存放的第一个数据存放进去
- * 3.2如果不是一次存放，那么每一次入栈的元素需要和栈顶元素进行比较，如果栈顶元素小，则入栈，如果比栈顶元素哒，那么不入栈
- * 出栈：
- * 1.需要判断stack栈的数据，在minStack当中是否存在，需要和栈顶元素进行比较，如果相等，minstack和stack都需要出栈
+ * 石头与宝石：
+ * 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ *
+ * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+ *
+ * 输入: J = "aA", S = "aAAbbbb"
+ * 输出: 3
+ *
+ * 输入: J = "z", S = "ZZ"
+ * 输出: 0
  */
-//首先需要两个栈，一个stack  一个minStack
-    class getMinStack{
-        private Stack<Integer> stack;
-        private Stack<Integer> minStack;
-
-        public getMinStack(){
-            this.stack = new Stack<>();
-            this.minStack = new Stack<>();
-        }
-    public void push(int x) {
-        //stack里面每一次都会存放数据
-        stack.push(x);
-        //如果minStack里面为空，那么直接将这个数据存放进去
-        if (minStack.empty()) {
-            minStack.push(x);
-        } else {
-            //如果不为空，将这个数据和minStack栈顶元素进行比较，如果x<=minStack.peek(),那么直接放进去
-            if (x <= minStack.peek()) {
-                minStack.push(x);
-            }
-        }
-    }
-    //查看栈顶元素并且删除
-    //stack中每次弹出的数据需要和minStack中的栈顶元素比较，因为如果Stack中要弹出的数据和minStack的数据一样
-    //那么minStack中栈顶元素也需要弹出，因为他的最小值发生了变化
-    public void pop() {
-            if(!stack.empty()) {
-                int e = stack.pop();
-                if (e == minStack.peek()) {
-                    e = minStack.pop();
-                }
-            }
-    }
-//获取栈顶元素，但是不删除
-    public int top() {
-        if (stack.isEmpty()) {
-            throw new RuntimeException("栈为空");
-        }
-        return stack.peek();
-    }
-
-    public int getMin() {
-        return minStack.peek();
-    }
-}
 public class TestDemo10 {
-    public static void main(String[] args) {
-        getMinStack getminStack = new getMinStack();
-        getminStack.push(3);
-        getminStack.push(-12);
-        getminStack.push(-15);
-        getminStack.push(13);
-        getminStack.push(5);
-        System.out.println(getminStack.top());
-        System.out.println(getminStack.getMin());
 
+    /**
+     * 思路方法，
+     * 既然说了J中的数据不重复，那么我们就可以将其放入到set集合当中，然后在遍历S，定义一个count，如果存在就++，最后返回count
+     * @param jewels
+     * @param stones
+     * @return
+     */
+    public int numJewelsInStones(String jewels, String stones) {
+        //因为set集合存放的数据是不重复的，所以用set集合
+        Set<Character> set = new HashSet<>();
+        //首先将jewls转换成字符数组
+        char[] chars = jewels.toCharArray();
+        //然后遍历将其放入到set中
+        for (char s: chars
+             ) {
+            set.add(s);
+        }
+        //然后定义一个count，然后开始遍历S
+        int count = 0;
+        for (char s: stones.toCharArray()
+             ) {
+            if(set.contains(s)){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String jewls = "aA";
+        String stones = "aAAbbb";
+        TestDemo10 testDemo10 = new TestDemo10();
+        int ret = testDemo10.numJewelsInStones(jewls,stones);
+        System.out.println(ret);
     }
 }
