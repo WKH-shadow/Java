601a4f4f2473ca571d6ce3809a564a35daea2d07
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-06-20 20:18:44 +0800

    只出现一次的数字

--- TestDemo8.java
+++ TestDemo8.java
@@ -1,111 +1,54 @@
-package JavaPractice;
+package TestDemo;
 
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
- * 队列实现栈：
- * 方法：
- * 1.首先需要两个队列
- * 2.每次将数放入不为空的队列里面
- * 3.当需要出栈的时候，就只需要将这个数的前n个数全部出队列到另一个空的队列里面，然后在将需要出栈的数字从队列里面弹出来
+ * 只出现一次的数字
  */
-
-class MyQueueStack{
-    //首先需要两个队列，里面就放Integer
-   private Queue<Integer> queue1;
-   private Queue<Integer> queue2;
-   private int UsedSize;
-   //提供构造方法
-
-    public MyQueueStack() {
-        this.queue1 = new LinkedList<>();
-        this.queue2 = new LinkedList<>();
-        this.UsedSize = 0;
-    }
-    /**
-     * 如果两个队列都为空，那么规定将数字放入到queue1当中
-     * 如果不为空，那么每次将数字放入到不为空的队列里面
-     */
-    public void push(int x) {
-        if(!queue1.isEmpty()){
-            queue1.offer(x);
-        }else if(!queue2.isEmpty()){
-            queue2.offer(x);
-        }else {
-            queue1.offer(x);
-        }
-        this.UsedSize++;
-    }
-
-    /**
-     * pop时查看栈顶元素也就时出栈
-     * 出栈：
-     * 1.如果栈为空，也就是两个队列都为空，那么就抛出空指针异常
-     * 2.如果不为空，那么需要将不为空的队列出到只剩下一个数
-     */
-    public int pop() {
-        if(queue1.isEmpty()&&queue2.isEmpty()){
-            throw new RuntimeException("空指针异常");
-        }
-        int e = 0;
-        if(!queue1.isEmpty()){
-            for (int i = 0; i < this.UsedSize-1; i++) {
-             queue2.offer(queue1.poll());
-            }
-            e = queue1.poll();
-        }else {
-            for (int i = 0; i < this.UsedSize-1; i++) {
-                queue1.offer(queue2.poll());
-            }
-            e = queue2.poll();
-        }this.UsedSize--;
-        return e;
-    }
-
-
-    /** Get the top element.
-     * 查看栈顶元素但是不删除
-     * 方法：
-     * 1.如果栈为空，也就是两个队列都为空，那么就抛出空指针异常
-     * 2.如果不为空，那么需要将不为空的队列所有的元素出到为空的队列里面，但是每一次出队列的
-     * 数字都需要被临时记录下来，也就是定义一个临时的变量，把每一次出出来的值放在里面，知道出出来的数字时最后一个，
-     * 然后返回出来该临时变量就是我们所需要查看的值
-     * @return
-     */
-    public int top() {
-        if (empty()) {
-            throw new RuntimeException("空指针异常");
-        }
-        int e = 0;
-        if (!queue1.isEmpty()) {
-            for (int i = 0; i < this.UsedSize; i++) {
-                e = queue1.poll();
-                queue2.offer(e);
-            }
-        } else {
-            for (int i = 0; i < this.UsedSize; i++) {
-                e = queue2.poll();
-                queue1.offer(e);
-            }
-        }
-        return e;
-    }
-    /** Returns whether the stack is empty. */
-    public boolean empty() {
-        return this.UsedSize == 0;
-    }
-}
-
 public class TestDemo8 {
-    public static void main(String[] args) {
-        MyQueueStack myQueueStack = new MyQueueStack();
-        myQueueStack.push(1);
-        myQueueStack.push(2);
-        myQueueStack.push(3);
-        myQueueStack.push(4);
-        System.out.println(myQueueStack.top());
+
+    //方法一：使用异或来解决，0异或任何数字都是任何数字，相同的数字异或出来都是0
+    //那么当把所有的数字都异或完毕之后，只剩下出现一次的那个数字就出来了
+    public int singleNumber1(int[] nums){
+        //定义一个ret来接受结果
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ret = ret ^ nums[i];
+        }
+        return ret;
     }
 
+    /**
+     *  方法二：使用set集合，遍历数组，然后当拿到一个数字的时候就在set里面判断
+     *  1.如果不在set集合里面的话，就直接将这数字放进set里面
+     *  2.如果在set集合的话，那么就直接使用move将set集合里面的这数字一出去
+     *  结果：到最后，set集合剩下来的那个数字就是只出现一次的数字
+     */
+
+    public int singleNumber(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])){
+                set.remove(nums[i]);
+            }else {
+                set.add(nums[i]);
+            }
+        }
+        int result = 0;
+        //set的一个特性，就是他里面存放的数字是无序的，所以要遍历来查找元素
+        for (int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])){
+                result = nums[i];
+                break;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,2,1};
+        TestDemo8 testDemo8 = new TestDemo8();
+        System.out.println(testDemo8.singleNumber(arr));
+        System.out.println("==================");
+        System.out.println(testDemo8.singleNumber1(arr));
+    }
 }
