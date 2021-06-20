2ce57ec7ec3578d76d1fb547136e8d1a0af120e3
WKH-shadow <64263890+WKH-shadow@users.noreply.github.com>
2021-06-20 20:19:03 +0800

    复制带随机指针的链表

--- TestDemo9.java
+++ TestDemo9.java
@@ -1,88 +1,73 @@
-package JavaPractice;
+package TestDemo;
 
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 栈实现队列
- * 方法：
- * 1.当然也是需要两个栈
- * 2.入队，如果两个栈都是空的，就把数字放在S1，每次都放在S1
- * 2.出都从S2出
- * 2.1.如果S2是空的，那么就把S1当中的元素全部导入到S2当中
- * 2.2.如果S2不是空的，直接将S2的栈顶元素进行pop()操作
+ * 复制带随机指针的链表
+ * 方法：使用HashMap
  */
-class MyStackQueue{
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
-    private int UsedSize;
-    public MyStackQueue(){
-        this.stack1 = new Stack<>();
-        this.stack2 = new Stack<>();
-        this.UsedSize = 0;
-    }
-    /** Push element x to the back of queue.
-     * 入队，如果两个栈都是空的，就把数字放在S1，每次都放在S1
-     */
-    public void push(int x) {
-        stack1.push(x);
-        this.UsedSize++;
-    }
+class Node {
+    int val;
+    Node next;
+    Node random;
 
-    /** Removes the element from in front of queue and returns that element.
-     * 1出都从S2出
-     * 1.1.如果S2是空的，那么就把S1当中的元素全部导入到S2当中
-     * 1.2.如果S2不是空的，直接将S2的栈顶元素进行pop()操作
-     */
-    public int pop() {
-        int e = 0;
-        if(stack2.empty()) {
-            while (!stack1.empty()){
-                stack2.push(stack1.pop());
-            }
-        }
-        if(!stack2.empty()){
-            e = stack2.pop();
-        }else {
-            System.out.println("队列为空");
-        }
-
-        return e;
-    }
-
-    /** Get the front element.
-     * 方法：
-     * 1.如果S2为空，s1不为空，那么就将栈S1的数据利用栈的性值全部放在S2，然后在返回S2的栈顶元素
-     * 2.如果S2不为空，那么就直接返回S2的栈顶元素
-     */
-    public int peek() {
-        if(stack2.empty()){
-            while (!stack1.isEmpty()){
-                stack2.push(stack1.pop());
-            }
-        }
-        int e = 0;
-        if(!stack2.empty()){
-            e = stack2.peek();
-        }else {
-            System.out.println("队列为空");
-        }
-        return e;
-    }
-
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-        if(stack1.empty()&&stack2.empty()){
-            return true;
-        }
-        return false;
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
 }
 public class TestDemo9 {
+    public Node copyRandomList(Node head) {
+        //首先需要一个HashMap来存放原节点，和复制之后所对应的节点
+        Map<Node,Node> map = new HashMap<>();
+        //首先先遍历一边单链表，然后现将简单的复制所有的节点，至于关系，后面在处理
+        //定义一个cur开始遍历节点
+        Node cur = head;
+        while (cur != null){
+            //此时创建一个节点，然后所对应的值就是cur的值
+            Node node = new Node(cur.val);
+            //然后将新旧节点对应关系放入到map当中
+            map.put(cur,node);
+            //然后cur接着往后遍历
+            cur = cur.next;
+        }
+        //此时Map里面存放的就是所对应的关系，也就是Map<原节点，新节点>,此时就处理他们之间的关系
+
+        //此时cur在重新指向原节点的头部
+        cur = head;
+        while (cur != null){
+            //首先处理next的关系，那也就是新节点的next就对应原节点的next
+            map.get(cur).next = map.get(cur.next);
+            //然后random的话就是新的节点的random就对应原先节点的random
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        //走到这里说明关系已经处理完毕，那么就然后新的链表的头结点，而新的链表的头结点也就是map表原节点对应的新节点head
+        return map.get(head);
+    }
     public static void main(String[] args) {
-        MyStackQueue myStackQueue = new MyStackQueue();
-        myStackQueue.push(1);
-        myStackQueue.push(2);
-        myStackQueue.push(3);
-        System.out.println(myStackQueue.peek());
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        Node cur1 = node1;
+        while (cur1 != null){
+            System.out.print(cur1.val+" ");
+            cur1 = cur1.next;
+        }
+        System.out.println();
+        System.out.println("====================");
+        TestDemo9 testDemo9 = new TestDemo9();
+        Node head = testDemo9.copyRandomList(node1);
+        Node cur = head;
+        while (cur != null){
+            System.out.print(cur.val+" ");
+            cur = cur.next;
+        }
     }
 }
